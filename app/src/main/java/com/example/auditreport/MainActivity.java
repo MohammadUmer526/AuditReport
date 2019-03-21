ae97b289bcbc824820fcdff173407529abe21f62
unknown <Administrator@KHIPC-00432.Systemsltd.local>
2019-03-21 17:29:28 +0500

    Initial commit

---
+++ app/src/main/java/com/example/auditreport/MainActivity.java
@@ -0,0 +1,182 @@
+package com.example.auditreport;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+public class MainActivity extends AppCompatActivity{
+
+
+    // declare the variables
+    SessionManager sessionManager;
+    private EditText email, password;
+    private Button btn_login;
+    private ProgressBar loading;
+
+    //defining AwesomeValidation object
+    private AwesomeValidation awesomeValidation;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // initialize awsomevalidation's object
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+
+        // Setting variables
+
+        loading = findViewById(R.id.loading);
+        email = findViewById(R.id.u_name);
+        password = findViewById(R.id.password);
+        btn_login = findViewById(R.id.btn_login);
+        //btnlog_out = findViewById(R.id.log_out);
+        TextView link_register = findViewById(R.id.link_regist);
+
+        //instance of SessionManager
+        sessionManager = new SessionManager(this);
+
+
+        awesomeValidation.addValidation(this, R.id.u_name,
+                                        "^[A-Za-z\\s]{1,}[\\.]{0,1}[A-Za-z\\s]{0,}$",
+                                          R.string.nameerror);
+
+
+
+
+
+        // when click on log in check the credentials
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mEmail = email.getText().toString().trim();
+                String mPass = password.getText().toString().trim();
+
+                if(!mEmail.isEmpty() || !mPass.isEmpty()){
+                    logIn(mEmail,mPass);
+                }else {
+                    email.setError("Please insert an user name");
+                    password.setError("Please insert a password");
+                }
+            }
+
+
+
+
+
+        });
+        // intent to register activity
+        link_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             //   startActivity(new Intent(MainActivity.this, Registration.class));
+            }
+        });
+
+    }
+
+        //LogIn method to perform login's functionalities
+    public void logIn(final String email, final  String password){
+        loading.setVisibility(View.VISIBLE);
+        btn_login.setVisibility(View.GONE);
+
+        String URL_LOGIN = "" ;
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_LOGIN,
+                new Response.Listener<String>() {
+
+                    @Override
+
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("Success");
+                            JSONArray jsonArray = jsonObject.getJSONArray("login");
+
+                            if (success.equals("1")) {
+                                for (int i = 0; i < jsonArray.length(); i++) {
+                                    JSONObject object = jsonArray.getJSONObject(i);
+                                    String name = object.getString("name").trim();
+                                    String email = object.getString("email").trim();
+                                    String id = object.getString("id").trim();
+
+
+                                    Toast.makeText(MainActivity.this, "Success Login" +
+                                                    " \nYour Name: " + name + " \nYour Email: " + email,
+                                            Toast.LENGTH_SHORT).show();
+                                    sessionManager.createSession(name, email, id);
+
+                                    loading.setVisibility(View.GONE);
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            loading.setVisibility(View.GONE);
+                            btn_login.setVisibility(View.VISIBLE);
+                            Toast.makeText(MainActivity.this, "Error" + e.toString(),
+                                            Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        loading.setVisibility(View.GONE);
+                        btn_login.setVisibility(View.VISIBLE);
+                        Toast.makeText(MainActivity.this, "Error"+ error.toString(),
+                                        Toast.LENGTH_SHORT).show();
+                    }
+
+
+                       }
+                    )
+
+        {
+
+            // putting the valid credentials
+            @Override
+            protected Map<String, String> getParams(){
+                Map<String,String> params = new HashMap<>();
+                params.put("email",email);
+                params.put("password", password);
+                return  params;
+            }
+
+                };
+
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+                                    requestQueue.add(stringRequest);
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/auditreport/SessionManager.java
@@ -0,0 +1,64 @@
+package com.example.auditreport;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import java.util.HashMap;
+
+public class SessionManager {
+
+    SharedPreferences sharedPreferences;
+    public SharedPreferences.Editor editor;
+    private Context context;
+    private static final String PREF_NAME = "LOGIN";
+    private static final String LOGIN = "IS_LOGIN";
+    public static final String NAME = "NAME";
+    public static final String EMAIl = "EMAIL";
+    public static final String ID = "ID";
+
+    public SessionManager(Context context){
+        this.context = context;
+        int PRIVATE_MODE = 0;
+        sharedPreferences = context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = sharedPreferences.edit();
+
+    }
+
+    public void createSession(String name, String email, String id){
+        editor.putBoolean(LOGIN, true);
+        editor.putString(NAME, name);
+        editor.putString(EMAIl,email);
+        editor.putString(ID, id);
+        editor.apply();
+    }
+
+    private boolean isLoggin(){
+        return sharedPreferences.getBoolean(LOGIN,false);
+    }
+    public void checkLogin(){
+        if(!this.isLoggin()){
+            Intent ab = new Intent(context, MainActivity.class);
+            context.startActivity(ab);
+            ((MainActivity) context).finish();
+        }
+    }
+
+    public HashMap<String, String> getUserDetail(){
+        HashMap<String, String> user = new HashMap<>();
+        user.put(NAME, sharedPreferences.getString(NAME, null));
+        user.put(EMAIl,sharedPreferences.getString(EMAIl,null));
+        user.put(ID,sharedPreferences.getString(ID,null));
+        return  user;
+
+    }
+
+
+    public void logout(){
+        editor.clear();
+        editor.commit();
+        Intent lg  = new Intent(context, MainActivity.class);
+        context.startActivity(lg);
+        ((MainActivity)context).finish();
+    }
+}
